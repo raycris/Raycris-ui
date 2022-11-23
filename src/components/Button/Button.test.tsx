4b198d30b2033133aad0e0d12c5de850d0fb3daa
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-11-22 21:24:00 -0400

    jest configuration done

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Button from "./Button";
+
+describe("Button", () => {
+  test("render the Button component", () => {
+    render(
+      <Button label="add something" background="#e16b31" colorLabel="#FFFFFF" />
+    );
+  });
+});
